c81de4990439331588ee35d5c0e5f818ef585a54
Nicole Gilbert <nicolegilbert16@gmail.com>
2017-10-31 12:13:19 -0700

    second higher order drill

---
+++ high_order_functions_02.js
@@ -0,0 +1,25 @@
+// Return only names that begin with 'R'
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+const name = '';
+const filteredNames = filter(myNames, function(name) {
+  // This is a "predicate function" - it's a function that only returns a boolean
+  return name[0] === 'R';
+});
+
+
+console.log(filter(myNames, fn)); // => ['Rich', 'Ray']
+
+// TASK -- DEFINE YOUR FILTER FUNCTION BELOW:
+function filter(arr, fn) {
+  let newArray = [];
+  for (i=0; i<arr.length; i++) {
+    if(fn(arr[i])===true) {
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
+}
+
+function fn(name){
+  return name[0] === 'R';
+}
